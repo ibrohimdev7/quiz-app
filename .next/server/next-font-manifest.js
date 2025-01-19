2157f61643bb6be9878d94a8826304d592bd9468
Ibrohim Qahramonov <qahramonovibrohimdev@gmail.com>
2025-01-19 17:20:48 +0500

    add next folder

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/a1/Desktop/Univer/Programming/quiz-app/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
